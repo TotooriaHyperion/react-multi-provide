f6f518393e365ee814af0dfb48bae836b0ca1a7c
hanyinya <hanyinya@bytedance.com>
2020-12-15 14:47:42 +0800

    fix: allow same symbol to get same identifier

--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,18 @@
 import { ProvidersViewModel } from "./types";
 
+// this would create TS7053 error, issue is raised at typescript#1863
+// const idMap: Record<symbol, ProvidersViewModel.ContextIdentifier<any>> = {};
+const idMap: any = {};
+
 export function createId<T>(
   symbol: symbol,
 ): ProvidersViewModel.ContextIdentifier<T> {
-  return {
-    [Symbol.toStringTag]: () => symbol.toString(),
-    toString: () => symbol.toString(),
-    valueOf: () => symbol.toString(),
-  };
+  if (!idMap[symbol]) {
+    idMap[symbol] = {
+      [Symbol.toStringTag]: () => symbol.toString(),
+      toString: () => symbol.toString(),
+      valueOf: () => symbol.toString(),
+    };
+  }
+  return idMap[symbol];
 }
